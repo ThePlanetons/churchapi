5dd32dd9821c3c9eaa415f7d0c86dc78c62c3943
MathanSekar <mathan.sekar@outlook.com>
2025-01-08 15:22:38 +0530

    JWT Setup

--- auth_app/api/serializers.py
+++ auth_app/api/serializers.py
@@ -1,7 +1,26 @@
-# from rest_framework import serializers
-# from member_app.models import member
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
-# class MemberSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = member
-#         fields = '__all__'
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        # Call the original validate method to get the tokens
+        data = super().validate(attrs)
+
+        # Format the response to include tokens under the 'token' key
+        data['token'] = {
+            'refresh': data.pop('refresh'),
+            'access': data.pop('access'),
+        }
+
+        # Add custom user data to the response
+        data.update({
+            'id': self.user.id,
+            'username': self.user.username,
+            'email': self.user.email,
+            'first_name': self.user.first_name,
+            'last_name': self.user.last_name,
+            'is_active': self.user.is_active,
+            'name': f"{self.user.first_name} {self.user.last_name}",
+            'last_login': self.user.last_login,
+        })
+
+        return data
--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
+from .views import CustomTokenObtainPairView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
 urlpatterns = [
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 # urlpatterns = [
