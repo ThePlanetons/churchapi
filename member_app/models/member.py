e8638ece096aaa4b62c4551567ae2bf2e7767c76
MathanSekar <mathan.sekar@outlook.com>
2025-01-30 16:53:00 +0530

    Member dynamic_fields column added

--- member_app/models/member.py
+++ member_app/models/member.py
@@ -9,10 +9,12 @@ class member(models.Model):
     phone = models.CharField(max_length=20)
     gender = models.CharField(max_length=20)
 
-    created_on = models.DateTimeField(auto_now_add=True)
-    created_by = models.CharField(max_length=100, null=True)
-    updated_on = models.DateTimeField(auto_now=True)
-    updated_by = models.CharField(max_length=100, null=True)
+    dynamic_fields = models.JSONField(default=dict)  
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.CharField(max_length=250, null=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    updated_by = models.CharField(max_length=250, null=True)
 
     def __str__(self):
         return f"{self.id}, {self.first_name} {self.last_name}"
--- member_app/models/member_config.py
+++ member_app/models/member_config.py
@@ -4,10 +4,10 @@ from django.db import models
 class member_config(models.Model):
     dynamic_input = models.JSONField()
 
-    created_on = models.DateTimeField(auto_now_add=True)
-    created_by = models.CharField(max_length=100, null=True)
-    updated_on = models.DateTimeField(auto_now=True)
-    updated_by = models.CharField(max_length=100, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.CharField(max_length=250, null=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    updated_by = models.CharField(max_length=250, null=True)
 
     def __str__(self):
         return f"{self.created_on} - {self.created_by}"
