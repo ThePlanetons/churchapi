122971545ea5731e56faa01a9423725e8ae2e8e1
MathanSekar <mathan.sekar@outlook.com>
2025-03-05 04:02:48 +0530

    Remove unused database connection parameters from settings

--- churchapi/settings.py
+++ churchapi/settings.py
@@ -107,9 +107,9 @@ import dj_database_url
 if os.environ.get("DATABASE_URL"):
     DATABASES = {
         "default": dj_database_url.config(
-            default=os.environ.get("DATABASE_URL"),
-            conn_max_age=600,
-            ssl_require=True
+            default=os.environ.get("DATABASE_URL")
+            # conn_max_age=600,
+            # ssl_require=True
         )
     }
 else:
