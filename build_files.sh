17b8c19d84ecbb48dd502e84515c711d2d8ac7d7
MathanSekar <mathan.sekar@outlook.com>
2025-03-05 02:28:23 +0530

    Update build_files.sh to modify Python and Pip PATH

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Install pip
-curl -sS https://bootstrap.pypa.io/get-pip.py | python3
+# Add Python and Pip to PATH
+export PATH="/python312/bin:$PATH"
 
 # Install dependencies
 pip install -r requirements.txt
