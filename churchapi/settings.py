f11a84812f49b657845320cc4d891c1be62a265a
MathanSekar <mathan.sekar@outlook.com>
2025-03-05 02:37:43 +0530

    Update database engine in settings to use the latest PostgreSQL backend

--- churchapi/settings.py
+++ churchapi/settings.py
@@ -113,7 +113,7 @@ DATABASES = {
     #     'PORT': '5432',
     # }
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ.get("DB_NAME"),
         "USER": os.environ.get("DB_USER"),
         "PASSWORD": os.environ.get("DB_PASSWORD"),
