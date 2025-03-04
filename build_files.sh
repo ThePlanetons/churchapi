fe56cc3ea7a65cafe1374fdaa40a522ddd7bee96
MathanSekar <mathan.sekar@outlook.com>
2025-03-05 02:30:12 +0530

    Add pip installation to build script and update static files collection comment

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Install pip
+curl -sS https://bootstrap.pypa.io/get-pip.py | python3
+
 # Add Python and Pip to PATH
 export PATH="/python312/bin:$PATH"
 
 # Install dependencies
 pip install -r requirements.txt
 
-# Collect static files
+# Collect Static Files
 python manage.py collectstatic --noinput
