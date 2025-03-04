c1286fce554eb272480352b92add15cf401ec4de
MathanSekar <mathan.sekar@outlook.com>
2025-03-05 02:31:20 +0530

    Update build_files.sh to use python3 for dependency installation and static file collection

--- build_files.sh
+++ build_files.sh
@@ -6,8 +6,8 @@ curl -sS https://bootstrap.pypa.io/get-pip.py | python3
 # Add Python and Pip to PATH
 export PATH="/python312/bin:$PATH"
 
-# Install dependencies
+# Use python3 instead of python
 pip install -r requirements.txt
 
 # Collect Static Files
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
